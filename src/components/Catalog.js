a4c4d538658cc91973097d5a580cd918763bafd6
Lorenzo <lorenzo.scafetta@gmail.com>
2023-12-15 11:32:11 +0100

    fix menù hamburger

--- src/components/Catalog.js
+++ src/components/Catalog.js
@@ -1,94 +1,81 @@
-import React, { useState, useEffect } from 'react';
-import { Card, Row, Col, Select, InputNumber } from 'antd';
-import './antd/dist/antd.css';
+import React from 'react';
+import { Card, Row, Col, Select, InputNumber, Pagination } from 'antd';
 
 const { Option } = Select;
 
-const Catalog = () => {
-  const [products, setProducts] = useState([]);
-  const [filteredProducts, setFilteredProducts] = useState([]);
-  const [categories, setCategories] = useState([]);
-  const [selectedCategory, setSelectedCategory] = useState('');
-  const [priceRange, setPriceRange] = useState([0, 1000]);
+class Catalog extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            products: [],
+            categories: [],
+            filteredProducts: [],
+            selectedCategory: '',
+            priceRange: [0, 1000]
+        }
+    }
 
-  useEffect(() => {
-    // Fetch categories from the API
-    fetch('https://api.escuelajs.co/api/v1/categories')
-      .then((response) => response.json())
-      .then((data) => setCategories(data));
+    componentDidMount() {
+        let products = fetch('https://api.escuelajs.co/api/v1/products')
+            .then(res => {return res.json()});
+        let categories = fetch('https://api.escuelajs.co/api/v1/categories')
+            .then(res => {return res.json()});
+        products.then(data => this.setState({ products: data}));
+        categories.then(data => this.setState({ filteredProducts: data}));
+    }
 
-    // Fetch products from the API
-    fetch('https://api.escuelajs.co/api/v1/products')
-      .then((response) => response.json())
-      .then((data) => {
-        setProducts(data);
-        setFilteredProducts(data);
-      });
-  }, []);
+    handleCategoryChange = value => {
+        this.setState({selectedCategory: value});
+    }
 
-  useEffect(() => {
-    // Apply filters based on selected category and price range
-    const filtered = products.filter(
-      (product) =>
-        (selectedCategory === '' || product.category === selectedCategory) &&
-        product.price >= priceRange[0] &&
-        product.price <= priceRange[1]
-    );
-    setFilteredProducts(filtered);
-  }, [selectedCategory, priceRange, products]);
-
-  const handleCategoryChange = (value) => {
-    setSelectedCategory(value);
-  };
-
-  const handlePriceRangeChange = (value) => {
-    setPriceRange(value);
-  };
-
-  return (
-    <div>
-      <Row gutter={[16, 16]}>
-        <Col span={6}>
-          <Select
-            style={{ width: '100%' }}
-            placeholder="Select category"
-            onChange={handleCategoryChange}
-          >
-            <Option value="">All Categories</Option>
-            {categories.map((category) => (
-              <Option key={category.id} value={category.name}>
-                {category.name}
-              </Option>
-            ))}
-          </Select>
-        </Col>
-        <Col span={6}>
-          <InputNumber
-            style={{ width: '100%' }}
-            placeholder="Min Price"
-            onChange={(value) => handlePriceRangeChange([value, priceRange[1]])}
-          />
-        </Col>
-        <Col span={6}>
-          <InputNumber
-            style={{ width: '100%' }}
-            placeholder="Max Price"
-            onChange={(value) => handlePriceRangeChange([priceRange[0], value])}
-          />
-        </Col>
-      </Row>
-      <Row gutter={[16, 16]}>
-        {filteredProducts.map((product) => (
-          <Col key={product.id} xs={24} sm={12} md={8} lg={6}>
-            <Card title={product.name} style={{ width: '100%' }}>
-              <img src={product.images}></img>
-              <p>Category: {product.category.name}</p>
-            </Card>
-          </Col>
-        ))}
-      </Row>
-    </div>
-  );
-};
-
+    handlePriceRangeChange = value => {
+        this.setState({ priceRange: value });
+    }
+    render() {
+        let {categories, filteredProducts, priceRange} = this.state;
+        return (
+            <div>
+              <Row gutter={[16, 16]}>
+                <Col span={6}>
+                  <Select
+                    style={{ width: '100%' }}
+                    placeholder="Select category"
+                    onChange={this.handleCategoryChange}
+                  >
+                    <Option value="">All Categories</Option>
+                    {categories.map((category) => (
+                      <Option key={category.id} value={category.name}>
+                        {category.name}
+                      </Option>
+                    ))}
+                  </Select>
+                </Col>
+                <Col span={6}>
+                  <InputNumber
+                    style={{ width: '100%' }}
+                    placeholder="Min Price"
+                    onChange={(value) => this.handlePriceRangeChange([value, priceRange[1]])}
+                  />
+                </Col>
+                <Col span={6}>
+                  <InputNumber
+                    style={{ width: '100%' }}
+                    placeholder="Max Price"
+                    onChange={(value) => this.handlePriceRangeChange([priceRange[0], value])}
+                  />
+                </Col>
+              </Row>
+              <Row gutter={[16, 16]}>
+                {filteredProducts.map((product) => (
+                  <Col key={product.id} xs={24} sm={12} md={8} lg={6}>
+                    <Card title={product.name} style={{ width: '100%' }}>
+                      <img src={product.images}></img>
+                    </Card>
+                  </Col>
+                ))}
+              </Row>
+            </div>
+          );
+    }
+}
 export default Catalog;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import logo from '../allyouneedisshop.png';
+import logo from '../assets/allyouneedisshop.png';
 import iconCart from '../assets/icon-cart.svg';
 import iconHeart from '../assets/icon-heart.svg';
 import iconUser from '../assets/icon-user.svg';
@@ -9,45 +9,52 @@ import SearchBar from './SearchBar';
 import { Badge } from 'antd';
 
 
+const TopMenuBar = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 20px 10px;
+    align-items: center;
+    gap: 30px;
+    flex: 1 0 0;
+
+    @media (min-width: 768px) {
+        flex-direction: row;  // Torna a una direzione di riga per schermi più grandi
+        padding: 30px 104px;  // Ripristina il padding originale
+        text-align: center;  // Centra il testo per schermi più grandi
+        align-items: center; // Allinea verticalmente gli elementi con la SearchBar
+     }
+`;
+
 const Top = styled.div`
    display: flex;
-   flex-direction: column;  // Modifica la direzione in una colonna per schermi più piccoli
-   padding: 20px;  // Riduci il padding
+   flex-direction: column;
+   width: 1440px;
+   padding: 20px;
    align-items: center;
    gap: 10px;
+   text-align: center;  // Centra il testo per tutte le finestre di visualizzazione
 
    @media (min-width: 768px) {
-      flex-direction: row;  // Torna a una direzione di riga per schermi più grandi
-      padding: 0px 104px;  // Ripristina il padding originale
-   }
+    flex-direction: row;  // Torna a una direzione di riga per schermi più grandi
+    padding: 0px 104px;  // Ripristina il padding originale
+    text-align: left;  // Allinea il testo a sinistra per schermi più grandi
+ }
 `;
 
 const Center = styled.div`
    display: flex;
-   flex-direction: column;  // Modifica la direzione in una colonna per schermi più piccoli
-   padding: 20px 10px;  // Riduci il padding
+   width: 1440px
+   padding: 30px 104px;
    justify-content: space-between;
-   align-items: center;
-
-   @media (min-width: 768px) {
-      flex-direction: row;  // Torna a una direzione di riga per schermi più grandi
-      padding: 30px 104px;  // Ripristina il padding originale
-   }
+   align-items: center
 `;
 
-const TopMenuBar = styled.div`
-    display: flex;
-    padding: 10px 0px;
-    align-items: center;
-    gap: 30px;
-    flex: 1 0 0
-`;
 
 
 const Link = styled.a`
   color: black;
   text-decoration: none;
-  text-transform: ${props => props.upperCase ? 'uppercase' : ''}
+  text-transform: ${props => props.uppercase ? 'uppercase' :''};
 `;
 
 const Logo = styled.img`
@@ -66,12 +73,14 @@ const IconContainer = styled.div`
     height: 36px;
     flex-shrink: 0; 
 `;
+const Icon = styled.img`
+  padding-right: 10px;
+`;
 
 const badgeStyle = {
-    postion: 'absolute',
-    top: '-30px',
-    left: '-13px'
-}
+    position: 'absolute', top: '-25px'
+};
+
 
 const MenuIcon = styled.div`
   display: none;  // Nascondi il menu ad hamburger su schermi più grandi
@@ -93,63 +102,76 @@ const MobileMenu = styled.div`
   z-index: 1000;
 `;
 
+const DesktopIcons = styled.div`
+  @media (max-width: 767px) {
+    display: none; // Nascondi le icone desktop su schermi più piccoli
+  }
+`;
+
+const MobileIcons = styled.div`
+  display: none; // Nascondi l'icona hamburger su schermi più grandi
+
+  @media (max-width: 767px) {
+    display: block; // Mostra l'icona hamburger su schermi più piccoli
+  }
+`;
+
 class Header extends React.Component {
-
     constructor(props) {
-        super(props);
+        super(props)
         this.state = {
-          isMenuOpen: false,
+            isMenuOpen: false
         };
-      }
-    
-      toggleMenu = () => {
+    }
+    toggleMenu = () => {
         this.setState((prevState) => ({ isMenuOpen: !prevState.isMenuOpen }));
-      };
-
-
+    }
     render() {
-        const { isMenuOpen } = this.state;
-
+        const {isMenuOpen} = this.state;
         return(
         <div>
             <Top>
-                <TopMenuBar>
-                    <nav>
-                        <Link href="#">Chi siamo</Link> &nbsp;
-                        <Link href="#">Community</Link> &nbsp;
-                        <Link href="#">Brand</Link> 
-                    </nav>
-                </TopMenuBar>
-                ITA 
-            </Top>
-            <HR />
+          <TopMenuBar>
+            <nav>
+              <Link href="#">Chi siamo</Link> &nbsp;
+              <Link href="#">Community</Link> &nbsp;
+              <Link href="#">Brand</Link>
+            </nav>
+          </TopMenuBar>
+          <div style={{ margin: 'auto' }}>ITA</div>
+        </Top>
+        <HR />
             <Center>
-               <Logo src={logo} alt="all you need is shop" />
-               <SearchBar />
-                <MenuIcon onClick={this.toggleMenu}>
-                    <img src={iconHamburger} alt="Menu" style={{ maxWidth: '20%' }}/>
-                </MenuIcon>
-                <MobileMenu isOpen={isMenuOpen}>
-                    <img src={iconUser} alt="accedi" />
-                    <img src={iconHeart} alt="segna come preferito" />
-                    <img src={iconCart} alt="carrello" />
-                </MobileMenu>
-                {isMenuOpen ? null : (  // Aggiungi questa condizione per mostrare IconContainer solo quando il menu è chiuso
-                <IconContainer>
-                    <img src={iconUser} alt="accedi" />
-                    <img src={iconHeart} alt="segna come preferito" />
-                    <img src={iconCart} alt="carrello" />
-                    <Badge styles={badgeStyle} color="red" count="2" />
-                    </IconContainer>
-                )}
-            </Center>
+          <Logo src={logo} alt="all you need is shop" />
+          <SearchBar />
+          <MobileIcons>
+            <MenuIcon onClick={this.toggleMenu}>
+              <img src={iconHamburger} alt="Menu" style={{ maxWidth: '20%' }} />
+            </MenuIcon>
+            <MobileMenu isOpen={isMenuOpen}>
+              <img src={iconUser} alt="accedi" />
+              <img src={iconHeart} alt="segna come preferito" />
+              <img src={iconCart} alt="carrello" />
+            </MobileMenu>
+          </MobileIcons>
+          <DesktopIcons>
+            <IconContainer>
+              <Icon src={iconUser} alt="accedi" />
+              <Icon src={iconHeart} alt="segna come preferito" />
+              <span>
+                <Icon src={iconCart} alt="carrello" />
+                <Badge style={badgeStyle} color="red" count="2" />
+              </span>
+            </IconContainer>
+          </DesktopIcons>
+        </Center>
             <HR />
             <Top>
                 <nav>
-                    <Link upperCase href="#">Donna</Link> &nbsp;
-                    <Link upperCase href="#">Uomo</Link> &nbsp;
-                    <Link upperCase href="#">Bambini</Link> &nbsp;
-                    <Link upperCase href="#">Tutti gli articoli</Link> 
+                    <Link uppercase="uppercase" href="#">Donna</Link> &nbsp;
+                    <Link uppercase="uppercase" href="#">Uomo</Link> &nbsp;
+                    <Link uppercase="uppercase" href="#">Bambini</Link> &nbsp;
+                    <Link uppercase="uppercase" href="#">Tutti gli articoli</Link> 
                 </nav>
             </Top>
             <HR />
